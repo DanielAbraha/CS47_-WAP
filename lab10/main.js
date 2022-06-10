1dd608045495d1fd5db95ffba09faff069cdd24e
DanielAbraha <dannyabraha27@gmail.com>
2022-06-10 03:10:37 -0500

    lab10

--- lab10/main.js
+++ lab10/main.js
@@ -1,9 +1,9 @@
-import samrit from "./model/person.js"
+import person from "./model/person.js"
 import Employee from "./model/employee.js";
 const persons= [ 
-    new samrit("Anna Smith",new Date(1998,12,15)),
-    new samrit("Bob John",new Date(1945,11,16)),
-    new samrit("Carlos Slim Helu",new Date(1976,9,24))
+    new person("Anna Smith",new Date(1998,12,15)),
+    new person("Bob John",new Date(1945,11,16)),
+    new person("Carlos Slim Helu",new Date(1976,9,24))
 ]
 persons.forEach(e=>console.log(e.toString()));
 const jim = new Employee("Jim Hanson",new Date(1945,11,16),249990.00,new Date(2020,13,5));
